0ad5d5151759388cba45bdbb9e2970f253714b36
Sanket Gaikwad <sanketgaikwad@Sankets-MacBook-Air.local>
2024-05-09 09:07:36 +0530

    java file added

---
+++ FactorialCalculator.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class FactorialCalculator {
+    // Function to calculate factorial
+    public static int factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+
+        if (num < 0) {
+            System.out.println("Factorial is not defined for negative numbers.");
+        } else {
+            int result = factorial(num);
+            System.out.println("Factorial of " + num + " is " + result);
+        }
+
+        scanner.close();
+    }
+}
+
